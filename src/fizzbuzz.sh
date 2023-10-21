f545d5dbc9e94339963368837281adad120f37ff
katarn <katarn@katarn.es>
2023-10-21 12:48:46 +0200

    refactor: final fizzbuzz impl

--- src/fizzbuzz.sh
+++ src/fizzbuzz.sh
@@ -2,18 +2,16 @@
 
 number=$1
 
-if (( "$number" % 15 == 0 )); then
-  echo "FizzBuzz"
-  exit 0
-fi
-
 if (( "$number" % 3 == 0 )); then
-  echo "Fizz"
-  exit 0
+  echo -n "Fizz"
 fi
 
 if (( "$number" % 5 == 0 )); then
-  echo "Buzz"
+  echo -n "Buzz"
+fi
+
+if (( "$number" % 3 == 0 || "$number" % 5 == 0 )); then
+  echo
   exit 0
 fi
 
